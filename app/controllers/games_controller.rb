5c7fc8de40b5c50c2a716650308a66a1ef29c00c
benoit moret <benoit.moret@gmail.com>
2021-11-15 17:22:13 +0100

    Add game chiffreslettres

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,46 +2,52 @@ require 'open-uri'
 require 'json'
 # documentation :)
 class GamesController < ApplicationController
-def new
-  alphabet = ('A'..'Z').to_a
-  @letters = []
-  10.times { @letters << alphabet.sample }
-  # p letter_deck
-end
-
-def building?(guess, grid)
-  # p guess
-  # p grid
-  guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
-end
-
-def score
-  answer = params[:answer]
-  letters = params[:letters].downcase
-  # building?(answer, letters)
-  api_url = "https://wagon-dictionary.herokuapp.com/#{answer}"
-  dico_serialized = URI.open(api_url).read
-  dico = JSON.parse(dico_serialized)
-  # p dico
-  # p found = dico['found']
-  # p length = dico['length']
-  # p test = building?(answer, letters)
-
-# p letters.split('').join(',')
-
-  if (test && found)
-    @text_out = "Congratulations!! #{answer.upcase} is valid English Word!"
-  elsif test
-    @text_out = "Sorry but#{answer.upcase} does not seem to be valid English Word..."
-  else
-    @text_out = "Sorry but#{answer.upcase}  can't be built out of #{letters.upcase.split('').join(' ,')}"
+  def new
+    alphabet = ('A'..'Z').to_a
+    @letters = []
+    10.times { @letters << alphabet.sample }
+    # p letter_deck
   end
-end
 
-#   The word can’t be built out of the original grid
-# The word is valid according to the grid, but is not a valid English word
-# The word is valid according to the grid and is an English word
+  def building?(guess, grid)
+    # p guess
+    # p grid
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
 
+  def score
+    answer = params[:answer]
+    letters = params[:letters].downcase
 
-#F F F H V H N L O Y
+    dico = api(answer)
+
+    if building?(answer, letters)
+      return @text_out = "Congratulations!! #{answer.upcase} is valid English Word!" if dico['found']
+
+      @text_out = "Sorry but#{answer.upcase} does not seem to be valid English Word..."
+      # if dico['found']
+      #   @text_out = "Congratulations!! #{answer.upcase} is valid English Word!"
+      # else
+      #   @text_out = "Sorry but#{answer.upcase} does not seem to be valid English Word..."
+      # end
+    else
+      @text_out = "Sorry but#{answer.upcase}  can't be built out of #{letters.upcase.split('').join(' ,')}"
+    end
+
+    # if building?(answer, letters) && dico['found']
+    #   @text_out = "Congratulations!! #{answer.upcase} is valid English Word!"
+    # elsif building?(answer, letters)
+    #   @text_out = "Sorry but#{answer.upcase} does not seem to be valid English Word..."
+    # else
+    #   @text_out = "Sorry but#{answer.upcase}  can't be built out of #{letters.upcase.split('').join(' ,')}"
+    # end
+  end
+
+  private
+
+  def api(answer)
+    api_url = "https://wagon-dictionary.herokuapp.com/#{answer}"
+    dico_serialized = URI.open(api_url).read
+    JSON.parse(dico_serialized)
+  end
 end
