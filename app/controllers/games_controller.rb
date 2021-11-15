5f1251bafb136d5fcfc02cdd00540df7de952da9
benoit moret <benoit.moret@gmail.com>
2021-11-15 16:40:55 +0100

    Add game chiffreslettres

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,47 @@
+require 'open-uri'
+require 'json'
+# documentation :)
+class GamesController < ApplicationController
+def new
+  alphabet = ('A'..'Z').to_a
+  @letters = []
+  10.times { @letters << alphabet.sample }
+  # p letter_deck
+end
+
+def building?(guess, grid)
+  # p guess
+  # p grid
+  guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+end
+
+def score
+  answer = params[:answer]
+  letters = params[:letters].downcase
+  # building?(answer, letters)
+  api_url = "https://wagon-dictionary.herokuapp.com/#{answer}"
+  dico_serialized = URI.open(api_url).read
+  dico = JSON.parse(dico_serialized)
+  # p dico
+  # p found = dico['found']
+  # p length = dico['length']
+  # p test = building?(answer, letters)
+
+# p letters.split('').join(',')
+
+  if (test && found)
+    @text_out = "<strong>Congratulations!!</strong> #{answer} is valid English Word!"
+  elsif test
+    @text_out = "Sorry but<strong>#{answer}</strong> does not seem to be valid English Word..."
+  else
+    @text_out = "Sorry but<strong>#{answer}</strong>  can't be built out of #{letters.upcase.split('').join(' ,')}"
+  end
+end
+
+#   The word can’t be built out of the original grid
+# The word is valid according to the grid, but is not a valid English word
+# The word is valid according to the grid and is an English word
+
+
+#F F F H V H N L O Y
+end
